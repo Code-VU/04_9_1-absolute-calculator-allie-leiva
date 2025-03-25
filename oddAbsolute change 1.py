7100fd81c8009ffdb5c0b106ba647ded1cdef725
Allie Leiva <Allie.Leiva@vu.com>
2025-03-25 13:44:46 -0500

    attempt 2 fixed variable

--- oddAbsolute change 1.py
+++ oddAbsolute change 1.py
@@ -3,10 +3,10 @@ def calculateAbsolute():
     # This first line is provided for you
     in_num  = input("Enter a number: ")
 
-    if in(input_num) > 21:
-        diff =abs((int(in_num) - 21) * 2)
+    if in(in_num) > 21:
+        diff = abs((int(in_num) - 21) * 2)
     else:
-        Diff = abs(int(in_num) - 21)
+        diff = abs(int(in_num) - 21)
 
     print("Result:", diff)
     # end assignment
